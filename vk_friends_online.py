13dc1a55dcc0dec6e9309fde6272760e0517f0df
veean <nersesowm@gmail.com>
2016-12-25 03:05:39 +0400

    added funcs to get,  output friends

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,15 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5793481  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    return input("Please enter your VK login : ")
 
 
 def get_user_password():
-    pass
+    return getpass.win_getpass(prompt="Please enter your VK password : ")
 
 
 def get_online_friends(login, password):
@@ -17,16 +17,26 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_id = api.friends.getOnline()
+    friends_list = api.users.get(user_ids=friends_id)
+    return friends_list
 
 
-def output_friends_to_console(friends_online):
-    pass
+def output_friends_to_console(friends_online_):
+    for friend in friends_online_:
+        print("{} {} is online right now.".format(friend['first_name'], friend['last_name']))
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+
+    login_ = get_user_login()
+    password_ = get_user_password()
+
+    if login_ and password_:
+        friends_online = get_online_friends(login_, password_)
+        output_friends_to_console(friends_online)
+    else:
+        print("Invalid Login or Password !")
